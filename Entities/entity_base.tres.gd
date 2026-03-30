de3b76ed5662e7e817fff78c5e337ceaafa5d9af
Baltazar <pltka1235@gmail.com>
2026-03-30 18:41:53 +0200

    dorobene niektore dialogy s father_npc nie hotove

--- Entities/entity_base.tres.gd
+++ Entities/entity_base.tres.gd
@@ -5,9 +5,37 @@ class_name EntityBase
 @export var display_name: String = ""
 @export var can_interact: bool = true
 
+var is_moving = false
+var target_position
+var move_speed = 100
+
+signal move_finished
+
 func interact(interactor):
 	print(display_name + " interacted with by " + str(interactor.name))
 
 
 func set_can_interact(value: bool):
 	can_interact = value
+
+
+func move_to(pos: Vector2, speed: int):
+	target_position = pos
+	move_speed = speed
+	is_moving = true
+
+func _physics_process(delta):
+	if is_moving:
+		var direction = (target_position - global_position)
+		
+		if direction.length() < 2:
+			global_position = target_position
+			is_moving = false
+			velocity = Vector2.ZERO
+			move_finished.emit()
+			return
+		
+		velocity = direction.normalized() * move_speed
+		move_and_slide()
+	else:
+		velocity = Vector2.ZERO
--- EntityManager.gd
+++ EntityManager.gd
@@ -1,9 +1,18 @@
 extends Node
 
+
+
 func refresh_all_entities():
 	var entities = get_tree().get_nodes_in_group("entities")
-	
 	for entity in entities:
 		if entity.has_method("refresh_availability"):
 			print("entita sa refreshla:" + entity.entity_id)
 			entity.refresh_availability()
+
+func get_entity(entityId: String):
+	var entities = get_tree().get_nodes_in_group("entities")
+	for entity in entities:
+		if entity.entity_id == entityId:
+			return entity
+	print("entity not found: get_entity")
+	return null
